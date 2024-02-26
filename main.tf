3f7cadda7d5c29b4903287fd8331d715efe81f40
Fabrizio Tassone <fabrizio.tassone@almasoft.it>
2024-02-26 14:59:55 +0100

    ok

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ terraform {
 }
 
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  # host = "unix:///var/run/docker.sock"
+  host = var.DOCKER_SOCK
 }
 
 resource "docker_container" "nginx" {
